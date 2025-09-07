87111978b0d92b4c73c54a0d42265aba4d001b74
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-09-08 04:30:43 +0900

    :computer: bundle gem tint_me

---
+++ lib/tint_me.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tint_me/version"
+
+module TintMe
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tint_me/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TintMe
+  VERSION = "0.1.0"
+end
